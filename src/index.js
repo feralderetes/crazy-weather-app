7eef0d4dbb1d661e8fbdda53fc51034167eb0ecd
Fer Alderetes <43346557+feralderetes@users.noreply.github.com>
2024-01-23 16:21:47 +0100

    Refactored the design

--- src/index.js
+++ src/index.js
@@ -20,22 +20,34 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#city");
+  let countryElement = document.querySelector("#country");
   let conditionElement = document.querySelector("#condition");
+  let feelsLikeElement = document.querySelector("#feels-like");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let airPressureElement = document.querySelector("#air-pressure");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   let icon = document.querySelector("#icon");
 
   temperatureElement.innerHTML = temperature;
-  cityElement.innerHTML = response.data.city;
+  cityElement.innerHTML = `${response.data.city},`;
+  countryElement.innerHTML = `${response.data.country}`;
   conditionElement.innerHTML = response.data.condition.description;
+  feelsLikeElement.innerHTML = `${response.data.temperature.feels_like}°`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed} km/h`;
+  airPressureElement.innerHTML = `${response.data.temperature.pressure} hPa`;
   timeElement.innerHTML = formatDate(date);
   icon.innerHTML = `
     <img
@@ -57,6 +69,7 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input");
 
   searchCity(searchInput.value);
+  searchInput.value = "";
 }
 
 function getForecast(city) {
@@ -67,22 +80,29 @@ function getForecast(city) {
 
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-        <div class="weather-forecast-date">${day}</div>
-        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+        <div><img src="${
+          day.condition.icon_url
+        }" class="weather-forecast-icon" /></div>
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max">18°</span>
-          <span class="weather-forecast-temperature-min">12°</span>
+          <span class="weather-forecast-temperature-max">${Math.round(
+            day.temperature.maximum
+          )}°</span>
+          <span class="weather-forecast-temperature-min">${Math.round(
+            day.temperature.minimum
+          )}°</span>
         </div>
       </div>
     `;
+    }
   });
   forecastElement.innerHTML = forecastHtml;
 }
